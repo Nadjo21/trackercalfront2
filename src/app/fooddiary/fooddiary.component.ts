b56fb6afa041045c980504007122f1b674686dee
nadia j <n.jouini@free.fr>
2021-09-06 12:06:50 +0200

    correction recuperation appel api foodIntakeByDate

--- src/app/fooddiary/fooddiary.component.ts
+++ src/app/fooddiary/fooddiary.component.ts
@@ -24,20 +24,19 @@ export class FooddiaryComponent implements OnInit {
   // @ts-ignore
   selectedFoodList: Foodintake [] = [];
 
-  totalCalories=0;
+  totalCalories = 0;
   private selectedFood: any;
 
-  currentDate = new Date() ;
+  currentDate = new Date();
 
   //pour retrouver le détail d'un foodIntake sur une date selectionnée
   //date: any |undefined;
   //theDayBeforeDdate: any | undefined;
 
   // @ts-ignore
-  foodIntakeFoundByDate= this.api.getFoodIntakeByDate(this.date);
+  foodIntakeFoundByDate = this.api.getFoodIntakeByDate(this.date);
 
   private dateSelected: any;
-
 
 
   foodIntakeList = this.api.getFoodIntakeList();
@@ -47,7 +46,7 @@ export class FooddiaryComponent implements OnInit {
 
 
   ngOnInit(): void {
-console.log(this.currentDate);
+    console.log(this.currentDate);
 
   }
 
@@ -57,11 +56,13 @@ console.log(this.currentDate);
   changeFood($event: Event) {
 
     //je selectionne un aliment dans mon menu deroulant et je charge la liste de tous les aliments
-    this.api.getFoodList().subscribe(() => {
-      console.log('test changement selection');
+    this.api.getFoodList().subscribe(result => {
+      console.log(result);
+      // @ts-ignore
+      this.food = result;
       //je recupere le detail l'aliment selectionné
       this.food = this.foodDiaryForm.get('fooddetail')?.value;
-      console.log("Detail de mon objet food selectionné" + " " + this.food);
+      console.log(this.food);
 
     })
   }
@@ -88,13 +89,10 @@ console.log(this.currentDate);
 
     if (this.foodIntake) {
       this.selectedFoodList.push(this.foodIntake);
-
-
-
       //je calcule au fur et a mesure le total des calories en récupérant les valeurs de la cellule
       // @ts-ignore
-     this.totalCalories = this.totalCalories +(this.foodIntake.quantity * this.foodIntake.food.calories);
-     console.log("test "+ this.totalCalories);
+      this.totalCalories = this.totalCalories + (this.foodIntake.quantity * this.foodIntake.food.calories);
+      console.log("test " + this.totalCalories);
     }
 
   }
@@ -120,37 +118,34 @@ console.log(this.currentDate);
 
   }
 
-  addDay(daysIncrement : number) {
-  //je recupere la date selectionnée dans le calendrier
-    this.dateSelected=this.foodDiaryForm.get("date")?.value;
-console.log (this.dateSelected );
+  addDay(daysIncrement: number) {
+    //je recupere la date selectionnée dans le calendrier
+    this.dateSelected = this.foodDiaryForm.get("date")?.value;
+    console.log(this.dateSelected);
 //j'ajoute ou je supprime un jour au clic du bouton pour naviguer dans le calendrier
-  let previousOrFollowingDay = new Date (this.dateSelected) ;
-  previousOrFollowingDay.setDate(previousOrFollowingDay.getDate()+daysIncrement);
-    console.log("hier "+ previousOrFollowingDay);
+    let previousOrFollowingDay = new Date(this.dateSelected);
+    previousOrFollowingDay.setDate(previousOrFollowingDay.getDate() + daysIncrement);
+    console.log("hier " + previousOrFollowingDay);
 
-     //j'appelle l'API pour recuperer la liste des foodIntake sur cette journée
+    //j'appelle l'API pour recuperer la liste des foodIntake sur cette journée
 
+    this.api.getFoodIntakeByDate(this.dateSelected).subscribe(result => {
+      //je recupere le detail des foodintake trouvés dans le resultat
+      console.log(result);
+//je stocke le resultat
+      // @ts-ignore
+      this.foodIntake = result;
+      console.log(this.foodIntake);
 
-
-    // @ts-ignore
-    this.foodIntakeFoundByDate= this.api.getFoodIntakeByDate(this.dateSelected).subscribe( ()=>{
-      console.log("clic bouton " + previousOrFollowingDay  + "liste " + this.foodIntakeFoundByDate);
-
-
-
-     // si je trouve un foodIntake dont la date concorde avec la date selectionnée , je l'ajoute a la liste et je l'affiche
-         if (this.foodIntake ) {
-           // @ts-ignore
-           this.foodIntake=foodIntake;
-           console.log("foodIntake "+ this.foodIntake);
-           // @ts-ignore
-           this.selectedFoodList.push(this.foodIntake);}
-         console.log(this.selectedFoodList);
+      // si je trouve un foodIntake dont la date concorde avec la date selectionnée , je l'ajoute a la liste et je l'affiche
+      if (this.foodIntake) {
+        this.selectedFoodList.push(this.foodIntake);
+      }
+      console.log(this.selectedFoodList);
 
 
-   });
+    });
 
   }
 
- }
+}
