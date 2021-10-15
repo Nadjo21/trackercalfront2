b7413ebba47817db907a8aea0d53f29002604d48
nadia j <n.jouini@free.fr>
2021-10-15 15:12:50 +0200

    classes JWT copiées

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+
+import {FormBuilder, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ApiService} from "../api.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
+
+  constructor(private  api: ApiService, private formBuilder: FormBuilder, private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onConnect() {
+    this.api.signIn({
+      username: this.username?.value,
+      password: this.password?.value
+    }).subscribe(() => this.router.navigate(['']));
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
